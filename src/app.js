8d2142d0c8e36e5e22c4459aa90ab1e114dbc14c
darianpavlica <pavlica.darian@gmail.com>
2022-12-30 16:08:06 -0300

    subo repo

---
+++ src/app.js
@@ -0,0 +1,32 @@
+// Requerimos express y lo ejecutamos para tener disponibles todos los metodos que vamos a precisar
+const express = require("express");
+const app = express();
+
+// Modulo nativo para manejar las rutas de los archivos
+const path = require("path");
+
+// Usando recursos estáticos.
+app.use(express.static("public"));
+
+// Ponemos a escuchar el servidor
+app.listen(3033, () => {
+    console.log("Servidor corriendo en http://localhost:3033")
+});
+
+// Definimos las rutas a los distintos pedidos que nuestro sitio sabe responder
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
